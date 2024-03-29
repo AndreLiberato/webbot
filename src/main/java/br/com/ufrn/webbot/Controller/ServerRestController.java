f0b3565f34a44c631e9ef3885c229f71318635f3
Victor Xavier <victorxavier530@gmail.com>
2024-03-28 22:04:23 -0300

    Addition of program AB(bot)

--- src/main/java/br/com/ufrn/webbot/Controller/ServerRestController.java
+++ src/main/java/br/com/ufrn/webbot/Controller/ServerRestController.java
@@ -1,7 +1,9 @@
 package br.com.ufrn.webbot.Controller;
 
+import java.io.File;
 import java.util.Map;
 
+import org.alicebot.ab.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,18 +16,56 @@ import br.com.ufrn.webbot.Request.MessageRequest;
 @RequestMapping(path = "/server")
 public class ServerRestController {
 
-    private final Map<String, String> chatResponses = Map.of(
-            "olá", "Olá! Como posso ajudar?",
-            "como você está?", "Estou bem, obrigado por perguntar.",
-            "adeus", "Até logo!",
-            "qual é o seu nome?", "Meu nome é ChatBot.",
-            "quanto é 2 + 2?", "A resposta é 4.",
-            "qual é a capital do Brasil?", "A capital do Brasil é Brasília.");
-
     @PostMapping
     public ResponseEntity<String> receiveMessage(@RequestBody MessageRequest msg) {
         String question = msg.message().toLowerCase();
-        String response = chatResponses.getOrDefault(question, "Desculpe, não entendi sua pergunta.");
+        String response = responseFun(question);
         return ResponseEntity.ok(response);
+    }
+
+    private String responseFun(String inp){
+        String out = "";
+        try {
+            String resourcesPath = getResourcesPath();
+            System.out.println(resourcesPath);
+            MagicBooleans.trace_mode = false;
+            Bot bot = new Bot("super", resourcesPath);
+            Chat chatSession = new Chat(bot);
+            bot.brain.nodeStats();
+            String textLine = "";
+            textLine = inp;
+            if ((textLine == null) || (textLine.length() < 1))
+                textLine = MagicStrings.null_input;
+            if (textLine.equals("q")) {
+                System.exit(0);
+            } else if (textLine.equals("wq")) {
+                bot.writeQuit();
+                System.exit(0);
+            } else {
+                String request = textLine;
+                if (MagicBooleans.trace_mode)
+                    System.out.println(
+                            "STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
+                                    + ":TOPIC=" + chatSession.predicates.get("topic"));
+                String response = chatSession.multisentenceRespond(request);
+                while (response.contains("&lt;"))
+                    response = response.replace("&lt;", "<");
+                while (response.contains("&gt;"))
+                    response = response.replace("&gt;", ">");
+                out = response;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out;
+    }
+
+    private String getResourcesPath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 2);
+        System.out.println(path);
+        String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+        return resourcesPath;
     }
 }
